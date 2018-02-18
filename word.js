f5337ca820d66759a9339fad3ee33cefc314d621
stoversa <stoversa@gmail.com>
2018-02-18 17:32:26 -0500

    Updating Word constructor. Not quite working the way it should.

--- word.js
+++ word.js
@@ -1,18 +1,21 @@
 var Letter = require("./letter");
 //Depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess:
-function Word(){
-    
+function Word(currentWord){
+    this.word = currentWord.split("");
     this.letters = []; //An array of new Letter objects representing the letters of the underlying word
-    this.populateWord = function(character){
-        this.letters.push(new Letter(character));
-    };
+    this.addLetter = function () {
+        var array = this.letters;
+        this.word.forEach(function (v){
+            array.push(new Letter(v));
+        })
+    }
     this.runString = function(){
         //A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-        this.returnChar();
+        this.letters.forEach(v => v.returnChar());
     };
     this.checkWord = function (character){
         //A function that takes a character as an argument and calls the guess function on each letter object(the second function defined in Letter.js)
-        this.check();
+        this.letters.forEach(v => v.check());
     };
 }
 
