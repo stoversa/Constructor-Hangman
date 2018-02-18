ffb3b7fbf2122a0b536d9820fdc8ec10cb37ad9b
stoversa <stoversa@gmail.com>
2018-02-18 09:17:11 -0500

    Added require

---
+++ index.js
@@ -0,0 +1,6 @@
+/*Contains the logic for the course of the game
+Randomly selects a word and uses the Word constructor to store it
+Prompts the user for each guess and keeps track of the user's remaining guesses
+*/
+var inquirer = require('inquirer');
+var Word = require("./word");
