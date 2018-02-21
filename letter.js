fa5acb074c563b55736e3e757bfbc310bea306a5
stoversa <stoversa@gmail.com>
2018-02-20 21:02:58 -0500

    Revised returnChar function

--- letter.js
+++ letter.js
@@ -2,13 +2,13 @@ function Letter(character){
    //this constructor should be able to either display an underlying character or a blank placeholder(such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
     this.character = character; //A string value to store the underlying character for the letter
     this.guessed = false; //A boolean value that stores whether that letter has been guessed yet
-    this.returnChar = function (guess) { 
+    this.returnChar = function () { 
         //A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-        if (guess === this.character){
+        if (this.guessed){
             return this.character;
         }
         else {
-            return " _ "
+            return " _ ";
         }
     };
     this.check = function (guess){
